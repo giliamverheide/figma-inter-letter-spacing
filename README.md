e4909e261374fe41f272ad6a88670ab9edd2553c
Giliam <verheide.giliam@gmail.com>
2020-03-16 16:31:59 +0100

    Added cover and plugin notifications on run.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Figma Inter Letter Spacing
 
+![](/cover.png)
+
 This Figma plugin will set the letter spacing of selected text layers according to the [Inter Dynamic Metrics](https://rsms.me/inter/dynmetrics/).
 
 ## Usage
 
-Select one or more text layers and select the plugin to set the letter spacing automatically.
+1. Select one or more text layers.
+2. Run `Inter Letter Spacing` from the Plugins menu.
 
-*Any layers that don't use the **Inter** font will be left unchanged.*
+_Any layers that don't use the **Inter** font will be left unchanged._
--- code.ts
+++ code.ts
@@ -1,29 +1,44 @@
-function setLetterSpacing(fontSize): LetterSpacing {
+const FONT_FAMILY = 'Inter';
 
-  const a = -0.0223, b = 0.185, c = -0.1745;
+function setLetterSpacing(fontSize): LetterSpacing {
+  const a = -0.0223,
+    b = 0.185,
+    c = -0.1745;
 
   return {
-    'value': parseFloat((fontSize * (a + b * Math.pow(Math.E, c * fontSize))).toFixed(2)),
-    'unit': 'PIXELS'
-  }
-
+    value: parseFloat(
+      (fontSize * (a + b * Math.pow(Math.E, c * fontSize))).toFixed(2)
+    ),
+    unit: 'PIXELS'
+  };
 }
 
+function run() {
+  let count = 0;
+  let message = '';
 
-for (const node of figma.currentPage.selection) {
-  if (node.type === 'TEXT') {
-    
-    const font = node.fontName as FontName;
+  for (const node of figma.currentPage.selection) {
+    if (node.type === 'TEXT') {
+      const font = node.fontName as FontName;
 
-    if (font.family === 'Inter') {
-      figma.loadFontAsync(font)
-      .then(() => {
-        node.letterSpacing = setLetterSpacing(node.fontSize)
-      })
+      if (font.family === FONT_FAMILY) {
+        count++;
+        figma.loadFontAsync(font).then(() => {
+          node.letterSpacing = setLetterSpacing(node.fontSize);
+        });
+      }
     }
-
   }
+
+  if (count === 1) {
+    message = `✅ Updated ${count} layer.`;
+  } else if (count > 1) {
+    message = `✅ Updated ${count} layers.`;
+  } else {
+    message = `Please select layers that use the 'Inter' font family.`;
+  }
+
+  figma.closePlugin(message);
 }
 
-
-figma.closePlugin();
+run();
